64036f6074f8b862b59c0d4f0d52f635837252db
Denis de Bernardy <ddebarnardy@yahoo.com>
2008-12-12 14:46:42 +0000

    import plugins and sem-theme
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/redirect-manager/trunk@1 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ redirect-manager-admin.php
@@ -0,0 +1,95 @@
+<?php
+
+class redirect_manager_admin
+{
+	#
+	# init()
+	#
+	
+	function init()
+	{
+		add_filter('sem_api_key_protected', array('redirect_manager_admin', 'sem_api_key_protected'));
+
+		add_action('admin_menu', array('redirect_manager_admin', 'meta_boxes'));
+	} # init()
+
+
+	#
+	# sem_api_key_protected()
+	#
+	
+	function sem_api_key_protected($array)
+	{
+		$array[] = 'http://www.semiologic.com/media/software/marketing/redirect-manager/redirect-manager.zip';
+		
+		return $array;
+	} # sem_api_key_protected()
+	
+	
+	#
+	# meta_boxes()
+	#
+	
+	function meta_boxes()
+	{
+		if ( current_user_can('edit_pages') || current_user_can('publish_posts') )
+		{
+			add_meta_box('redirect_manager', 'Redirect', array('redirect_manager_admin', 'entry_editor'), 'post');
+			add_meta_box('redirect_manager', 'Redirect', array('redirect_manager_admin', 'entry_editor'), 'page');
+			add_action('save_post', array('redirect_manager_admin', 'save_entry'));
+		}
+	} # meta_boxes()
+	
+	
+	#
+	# entry_editor()
+	#
+	
+	function entry_editor()
+	{
+		$post_ID = isset($GLOBALS['post_ID']) ? $GLOBALS['post_ID'] : $GLOBALS['temp_ID'];
+		
+		$value = '';
+		
+		if ( $post_ID > 0 )
+		{
+			$value = get_post_meta($post_ID, '_redirect_url', true);
+		}
+		
+		echo '<p>'
+			. '<input type="text" name="redirect_manager" size="58" tabindex="5" class="code" style="width: 90%;"'
+				. ' value="' . attribute_escape($value) . '"'
+				. ' />'
+			. '</p>';
+		
+		echo '<p>'
+			. 'Enter the url to which visitors of this entry should automatically be redirected.'
+			. '</p>';
+	} # entry_editor()
+	
+
+	#
+	# save_entry()
+	#
+
+	function save_entry($post_ID)
+	{
+		if ( current_user_can('edit_pages') || current_user_can('publish_posts') )
+		{
+			delete_post_meta($post_ID, '_redirect_url');
+			
+			$value = stripslashes($_POST['redirect_manager']);
+			$value = strip_tags($value);
+			$value = trim($value);
+			
+			if ( $value )
+			{
+				add_post_meta($post_ID, '_redirect_url' . $field, $value, true);
+			}
+		}
+	} # save_entry()
+} # redirect_manager_admin
+
+redirect_manager_admin::init();
+
+?>
---
+++ redirect-manager.php
@@ -0,0 +1,71 @@
+<?php
+/*
+Plugin Name: Redirect Manager
+Plugin URI: http://www.semiologic.com/software/marketing/redirect-manager/
+Description: Lets you manage redirects on your site without messing around with .htaccess files
+Author: Denis de Bernardy
+Version: 1.0
+Author URI: http://www.semiologic.com
+Update Service: http://version.semiologic.com/wordpress
+Update Tag: redirect_manager
+Update Package: http://www.semiologic.com/media/software/marketing/redirect-manager/redirect-manager.zip
+*/
+
+/*
+Terms of use
+------------
+
+This software is copyright Mesoconcepts Ltd, and is distributed under the terms of the Mesoconcepts license. In a nutshell, you may freely use it for any purpose, but may not redistribute it without written permission.
+
+http://www.mesoconcepts.com/license/
+**/
+
+
+class redirect_manager
+{
+	#
+	# init()
+	#
+
+	function init()
+	{
+		add_action('template_redirect', array('redirect_manager', 'redirect'), -1000000);
+	} # init()
+
+
+	#
+	# redirect()
+	#
+	
+	function redirect()
+	{
+		if ( is_singular() && !is_feed() )
+		{
+			global $wp_query;
+			$post_id = $wp_query->get_queried_object_id();
+			
+			if ( $url = get_post_meta($post_id, '_redirect_url', true) )
+			{
+				header('HTTP/1.1 301 Moved Permanently');
+				header('Status: 301 Moved Permanently');
+				
+				global $is_IIS;
+				if ( $is_IIS ) {
+					header("Refresh: 0;url=$url");
+				} else {
+					header("Location: $url");
+				}
+				die;
+			}
+		}
+	}
+} # redirect_manager
+
+redirect_manager::init();
+
+
+if ( is_admin() )
+{
+	include dirname(__FILE__) . '/redirect-manager-admin.php';
+}
+?>
