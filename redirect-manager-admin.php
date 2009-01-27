31c62a03d95759c3bb1e4ab6d5e7255eeb0663b2
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-01-27 18:43:37 +0000

    Redirect Manager:
    - code clean-up
    - add security sanitation
    - php docs
    - pot file
    
    
    
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/redirect-manager/trunk@829 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- redirect-manager-admin.php
+++ redirect-manager-admin.php
@@ -1,45 +1,34 @@
 <?php
 
-class redirect_manager_admin
-{
-	#
-	# init()
-	#
+class redirect_manager_admin {
+	/**
+	 * meta_boxes()
+	 *
+	 * adds meta boxes to post and page editors
+	 *
+	 * @return void
+	 **/
 	
-	function init()
-	{
-		add_action('admin_menu', array('redirect_manager_admin', 'meta_boxes'));
-	} # init()
-	
-	
-	#
-	# meta_boxes()
-	#
-	
-	function meta_boxes()
-	{
-		if ( current_user_can('edit_pages') || current_user_can('publish_posts') )
-		{
-			add_meta_box('redirect_manager', 'Redirect', array('redirect_manager_admin', 'entry_editor'), 'post');
-			add_meta_box('redirect_manager', 'Redirect', array('redirect_manager_admin', 'entry_editor'), 'page');
-			add_action('save_post', array('redirect_manager_admin', 'save_entry'));
-		}
+	function meta_boxes() {
+		if ( current_user_can('edit_posts') )
+			add_meta_box('redirect_manager', __('Redirect', 'redirect-manager'), array('redirect_manager_admin', 'entry_editor'), 'post');
+		
+		if ( current_user_can('edit_pages') )
+			add_meta_box('redirect_manager', __('Redirect', 'redirect-manager'), array('redirect_manager_admin', 'entry_editor'), 'page');
 	} # meta_boxes()
 	
 	
-	#
-	# entry_editor()
-	#
+	/**
+	 * entry_editor($post)
+	 *
+	 * @param object $post the edited post
+	 * @return void
+	 * @author Denis
+	 **/
 	
-	function entry_editor()
-	{
-		$post_ID = isset($GLOBALS['post_ID']) ? $GLOBALS['post_ID'] : $GLOBALS['temp_ID'];
-		
-		$value = '';
-		
-		if ( $post_ID > 0 )
-		{
-			$value = get_post_meta($post_ID, '_redirect_url', true);
+	function entry_editor($post) {
+		if ( $post->ID > 0 ) {
+			$value = get_post_meta($post->ID, '_redirect_url', true);
 		}
 		
 		echo '<p>'
@@ -49,37 +38,38 @@ class redirect_manager_admin
 			. '</p>';
 		
 		echo '<p>'
-			. 'Enter the url to which visitors of this entry should automatically be redirected.'
+			. __('Enter the url to which visitors of this entry should automatically be redirected.', 'redirect-manager')
 			. '</p>';
 	} # entry_editor()
 	
-
-	#
-	# save_entry()
-	#
-
-	function save_entry($post_ID)
-	{
-		$post = get_post($post_ID);
+	
+	/**
+	 * save_entry($post_id)
+	 *
+	 * @param int $post_id the entry's ID (zero for a draft)
+	 * @return void
+	 * @author Denis
+	 **/
+	
+	function save_entry($post_id) {
+		if ( wp_is_post_revision($post_id) )
+			return;
 		
-		if ( $post->post_type == 'revision' ) return;
-		
-		if ( current_user_can('edit_pages') || current_user_can('publish_posts') )
-		{
-			delete_post_meta($post_ID, '_redirect_url');
+		if ( current_user_can('edit_post', $post_id) ) {
+			$value = $_POST['redirect_manager'];
+			$value = stripslashes($value);
+			$value = clean_url($value);
 			
-			$value = stripslashes($_POST['redirect_manager']);
-			$value = strip_tags($value);
-			$value = trim($value);
-			
-			if ( $value )
-			{
-				add_post_meta($post_ID, '_redirect_url', $value, true);
+			if ( $value ) {
+				update_post_meta($post_id, '_redirect_url', $value);
+			} else {
+				delete_post_meta($post_id, '_redirect_url');
 			}
 		}
 	} # save_entry()
 } # redirect_manager_admin
 
-redirect_manager_admin::init();
-
+# init
+add_action('admin_menu', array('redirect_manager_admin', 'meta_boxes'));
+add_action('save_post', array('redirect_manager_admin', 'save_entry'));
 ?>
--- redirect-manager.php
+++ redirect-manager.php
@@ -3,11 +3,12 @@
 Plugin Name: Redirect Manager
 Plugin URI: http://www.semiologic.com/software/marketing/redirect-manager/
 Description: Lets you manage redirects on your site without messing around with .htaccess files
-Version: 1.0.1
+Version: 1.1 alpha
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 Update Package: https://members.semiologic.com/media/plugins/redirect-manager/redirect-manager.zip
 */
+
 
 /*
 Terms of use
@@ -19,51 +20,74 @@ http://www.mesoconcepts.com/license/
 **/
 
 
-class redirect_manager
-{
-	#
-	# init()
-	#
+/**
+ * Redirect Manager
+ *
+ * @package Redirect Manager
+ * @author Denis
+ **/
 
-	function init()
-	{
-		add_action('template_redirect', array('redirect_manager', 'redirect'), -1000000);
-	} # init()
-
-
-	#
-	# redirect()
-	#
+class redirect_manager {
+	/**
+	 * redirect()
+	 *
+	 * Perform a redirect
+	 *
+	 * @return void
+	 * @author Denis
+	 **/
 	
-	function redirect()
-	{
-		if ( is_singular() && !is_feed() )
-		{
-			global $wp_query;
-			$post_id = $wp_query->get_queried_object_id();
-			
-			if ( $url = get_post_meta($post_id, '_redirect_url', true) )
-			{
-				header('HTTP/1.1 301 Moved Permanently');
-				header('Status: 301 Moved Permanently');
-				
-				global $is_IIS;
-				if ( $is_IIS ) {
-					header("Refresh: 0;url=$url");
-				} else {
-					header("Location: $url");
-				}
+	function redirect() {
+		if ( is_feed() || !is_singular() )
+		 	return;
+		
+		global $wp_query;
+		$post_id = $wp_query->get_queried_object_id();
+		
+		if ( $location = get_post_meta($post_id, '_redirect_url', true) ) {
+			if ( !current_user_can('edit_post', $post_id) ) {
+				wp_redirect($location, 301);
 				die;
+			} else {
+				add_filter('the_content', array('redirect_manager', 'display'));
 			}
 		}
-	}
+	} # redirect()
+	
+	
+	/**
+	 * display($content)
+	 *
+	 * Replace the content with a link to where the entry will perform a redirect
+	 *
+	 * @param string $content the entry's content
+	 * @return string $content
+	 * @author Denis
+	 **/
+	
+	function display($content) {
+		if ( is_feed() || !is_singular() )
+			return $content;
+		
+		global $wp_query;
+		$post_id = $wp_query->get_queried_object_id();
+		$location = get_post_meta($post_id, '_redirect_url', true);
+		$location = clean_url($location);
+		
+		if ( $location ) {
+			$caption = __('Redirects to <a href="%1$s">%2$s</a>', 'redirect-manager');
+			$content = wpautop(sprintf($caption, attribute_escape($location), $location));
+		}
+		
+		return $content;
+	} # display()
 } # redirect_manager
 
-redirect_manager::init();
+# init
+load_plugin_textdomain('redirect-manager', null, basename(dirname(__FILE__)) . '/lang');
+add_action('template_redirect', array('redirect_manager', 'redirect'), -1000000);
 
-
-if ( is_admin() )
-{
+if ( is_admin() ) {
 	include dirname(__FILE__) . '/redirect-manager-admin.php';
 }
 ?>
